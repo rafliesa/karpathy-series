3ee33c249e68042e65c44fe93ba5a66b9b70d053
rafliesa <rflisaa@gmail.com>
2025-09-10 13:09:15 +0700

    fit and predict

--- esagrad/nn.py
+++ esagrad/nn.py
@@ -13,11 +13,12 @@ class Neuron:
   def parameters(self):
     return self.w + [self.b]
 
+
 class Layer:
 
   def __init__(self, nin, nout):
     self.neurons = [Neuron(nin) for _n in range(nout)]
-  
+
   def __call__(self, x):
     outs = [n(x) for n in self.neurons]
     return outs[0] if len(outs) == 1 else outs
@@ -25,16 +26,38 @@ class Layer:
   def parameters(self):
     return [p for neuron in self.neurons for p in neuron.parameters()]
 
+
 class MLP:
 
-  def __init__(self, nin, nouts):
+  def __init__(self, nin, nouts, lr=0.01, epoch=20):
+    self.lr = lr
+    self.epoch = epoch
     sz = [nin] + nouts
     self.layers = [Layer(sz[i],sz[i+1]) for i in range(len(nouts))]
-  
-  def __call__(self, x):
+
+  def __forward__(self, x):
     for layer in self.layers:
       x = layer(x)
     return x
-  
+
   def parameters(self):
     return [p for layer in self.layers for p in layer.parameters()]
+
+  def fit(self, X, y):
+    for i in range(self.epoch):
+      ypred = [self.__forward__(x) for x in X]
+      loss = sum([(ygt-yout)**2 for ygt, yout in zip(y, ypred)])
+
+      loss.backward()
+
+      for p in self.parameters():
+        p.data -= self.lr * p.grad
+        p.grad = 0
+
+      print(i, loss.data)
+
+  def predict(self,X):
+    if len(X) == 1 :
+      return self.__forward__(X[0])
+
+    return [self.__forward__(x) for x in X]
